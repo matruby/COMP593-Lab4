187bf384dd29f93fd32313f6c6be6a50d34228dd
matruby <Matthew.Rubie@flemingcollege.ca>
2023-02-07 22:44:57 -0500

    Made sure everything worked properly and did final steps
    Created a final list with proper format of src:port and then sorted the list. Also did some debugging to make sure the first steps worked properly

--- find_port_scans.py
+++ find_port_scans.py
@@ -22,4 +22,17 @@ with open(r'C:\Users\rubes\COMP593-Lab4\gateway.log', 'r') as log_file:
     src = [re.search(src_srch_str, line).group() for  line in ps_lines]
     # Get matches of all the ports scanned
     port = [re.search(r'DPT=[0-9]+', line).group() for line in ps_lines]
+
+    # The final list will contain the proper format (src:port)
+    final_lst = [] 
+    for count in range(len(src)):
+        # Get just the source and the port
+        proper_src = re.search(r'[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+', src[count])
+        proper_port = re.search(r'[0-9]+', port[count])
+        # String with proper formatting
+        proper_str = f'{proper_src.group()}:{proper_port.group()}'
+        final_lst.append(proper_str)
+
+    # Sort the list in final_lst
+    final_lst.sort()
     
