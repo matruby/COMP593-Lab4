d61a91f978c62a1ab45b28264a41e3abb93d5db8
matruby <Matthew.Rubie@flemingcollege.ca>
2023-02-07 22:20:48 -0500

    Added next steps
    Matches the source adddresses and ports and adds them to a list

--- Lab4.py
+++ Lab4.py
@@ -16,4 +16,10 @@ with open(r'C:\Users\rubes\COMP593-Lab4\gateway.log', 'r') as log_file:
         ps_match = re.search(ps_srch_str, line)
         if ps_match != None:
             ps_lines.append(ps_match.group())
+
+    # Get matches of all the source addresses that scanned
+    src_srch_str = r'SRC=[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+'    
+    src = [re.search(src_srch_str, line).group() for  line in ps_lines]
+    # Get matches of all the ports scanned
+    port = [re.search(r'DPT=[0-9]+', line).group() for line in ps_lines]
     
