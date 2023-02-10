6565b816ca83a743b53a50fd438f8e4a82bb2e03
matruby <Matthew.Rubie@flemingcollege.ca>
2023-02-10 13:49:38 -0500

    Made some major changes to the script. Still have to take things out that arent needed.

--- lab4_script_template.py
+++ lab4_script_template.py
@@ -1,71 +1,37 @@
 
-import os
-import re
-import sys
-
+from log_analysis import  get_log_file_path_from_cmd_line, filter_log_by_regex
 
 def main():
     log_file = get_log_file_path_from_cmd_line()
     # Get the regex to be used
-    re_filter = input('What regex would you like to use on this file: ')
+    re_filter = str(input('What regex would you like to use on this file: '))
+    raw_re = repr(re_filter)[1:-1]
     default_settings = input('Would you like to use default settings for search (y/n): ')
     if default_settings == 'n':
         # Ask the user if they want it to be case sensitive
-        ignore_case = input('Would you like this match to be case sensitive (y/n): ')
-        True if ignore_case == 'y' else False
+        case_sens = input('Would you like this match to be case sensitive (y/n): ')
+        if case_sens == 'y':
+            case_sens = True
+        elif case_sens == 'n':
+            case_sens = False
         # Ask the user if they want printing of records that pass through the filter
-        records = input('Would you like to print records that pass through filter (y/n): ')
-        True if records == 'y' else False
+        rec = input('Would you like to print records that pass through filter (y/n): ')
+        if rec == 'y':
+            rec = True
+        elif rec == 'n':
+            rec = False
         # Ask the user if they want to print a summary sentence
-        summary = input('Wouldyou like to print a summary sentence (y/n): ')
-        True if summary == 'y' else False
+        summary = input('Would you like to print a summary sentence (y/n): ')
+        if summary == 'y':
+            summary = True
+        elif summary == 'n':
+            summary = False
         
         # Search result
-        result = filter_log_by_regex(log_file, re_filter, ignore_case, summary, records) 
+        result = filter_log_by_regex(log_file, raw_re, ignore_case=case_sens, print_summary=summary, print_records=rec) 
     else:
         # Search result 
-        result = filter_log_by_regex(log_file, re_filter)
-
-# TODO: Step 3
-def get_log_file_path_from_cmd_line():
-    """Gets command line argument and checks if it is valid""" 
-    # Possible path to log file
-    log_path = sys.argv
-    # Check if there is only 1 argument given, if the path given is a file and is a log file
-    if len(log_path) == 2 and os.path.isfile(log_path[1]) and log_path[1][-4:] == '.log':
-        return log_path[1]
-    else:
-        print("File doesn't exist\nOr file isn't a log file")
-        sys.exit()
-
-
-# TODO: Steps 4-7
-def filter_log_by_regex(log_file, regex, ignore_case=True, print_summary=False, print_records=False):
-    """Gets a list of records in a log file that match a specified regex."""
-    # Check if it is just the default settings
-    matched_lines = None
-     
-    # Open the log file in read mode 
-    with open(log_file, 'r') as read_file:
-        # Read all of the lines into a list called logs
-        logs = read_file.readlines()
-        # Check if ignore_case was used
-        if ignore_case == False:
-            matched_lines = [re.search(regex, log, re.I).group() for log in logs if re.search(regex, log, re.I) != None]
-        else:
-            matched_lines = [re.search(regex, log).group() for log in logs if re.search(regex, log) != None]
-        # Print records if argument was used
-        if print_records:
-            [print(entry) for entry in matched_lines]
-        # Print summary if argument was used
-        if print_summary: 
-            print(f'\nThere was {len(matched_lines)} lines that matched the regex!')
-            if ignore_case == False:
-                print('This search was case-insensitive')
-            else:
-                print('This search was case-sensitive')
-
-    return matched_lines
+        result = filter_log_by_regex(log_file, raw_re)
 
 # TODO: Step 8
 def tally_port_traffic(log_file):
---
+++ log_analysis.py
@@ -0,0 +1,56 @@
+
+import os 
+import re 
+import sys
+
+def get_log_file_path_from_cmd_line():
+    """Gets command line argument and checks if it is valid""" 
+    # Possible path to log file
+    log_path = sys.argv
+    # Check if there is only 1 argument given, if the path given is a file and is a log file
+    if len(log_path) == 2 and os.path.isfile(log_path[1]) and log_path[1][-4:] == '.log': 
+        return log_path[1] 
+    else: 
+        print("File doesn't exist\nOr file isn't a log file") 
+        sys.exit() 
+
+     
+def filter_log_by_regex(log_file, regex, ignore_case=True, print_summary=False, print_records=False): 
+    """Gets a list of records in a log file that match a specified regex."""  
+    # List of matching lines from regex
+    matched_lines = [] 
+    # Convert the regex to a raw string
+    raw_re = repr(regex)[1:-1] 
+    # Open the log file in read mode 
+    with open(log_file, 'r') as read_file:
+        # Read all of the lines into a list called logs
+        logs = read_file.readlines()
+        
+        # Look through all the logs line by line
+        for log in logs:
+            # See if the search is case-sensitive or case-insensitive
+            if ignore_case:
+                search_match = re.search(raw_re, log, re.I)
+            else:
+                search_match = re.search(raw_re, log)
+            
+            # Check that there is a match and if there is capture groups or not
+            if search_match and search_match.lastindex:
+                matched_lines.append(search_match.groups())
+            elif search_match and not search_match.lastindex:
+                matched_lines.append(search_match.group())
+    
+    # See if the user wants all the matches printed
+    if print_records == True:
+        [print(f"{line_num} - {line}") for line_num, line in enumerate(matched_lines)]
+    # See if th user wants a summary of all the matches printed
+    if print_summary == True:
+        print(f"There is a total of {len(matched_lines)} matches for this log file.")
+        print(f"The regex matching was case{'in' if ignore_case else ''}-sensitive.")
+    
+    return matched_lines
+
+
+
+
+
